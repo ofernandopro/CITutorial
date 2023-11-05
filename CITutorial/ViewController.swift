a026b1550e805bc1d2bd3398143f188fef25a9e6
fernando <fernando@lealapps.com>
2023-11-04 22:36:52 -0300

    Initializing project

--- CITutorial/ViewController.swift
+++ CITutorial/ViewController.swift
@@ -11,9 +11,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    func factorial(_ number: Int) -> Int {
+        if number == 0 {
+            return 1
+        } else {
+            return number * factorial(number - 1)
+        }
+    }
+    
+    func isPrime(_ number: Int) -> Bool {
+        if number <= 1 {
+            return false
+        }
+        for i in 2..<number {
+            if number % i == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func getFibonacci(_ n: Int) -> Int {
+        if n <= 1 {
+            return n
+        }
+        return getFibonacci(n - 1) + getFibonacci(n - 2)
+    }
+    
 }
 
--- CITutorialTests/CITutorialTests.swift
+++ CITutorialTests/CITutorialTests.swift
@@ -10,27 +10,24 @@ import XCTest
 
 final class CITutorialTests: XCTestCase {
 
+    let vc: ViewController = ViewController()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testFactorialOfZero() {
+        XCTAssertEqual(vc.factorial(0), 1, "Factorial of 0 should be 1")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testFactorialOfPositiveNumber() {
+        XCTAssertEqual(vc.factorial(5), 120, "Factorial of 5 should be 120")
     }
-
+    
+    func testFactorialOfNegativeNumber() {
+        XCTAssertNil(vc.factorial(-3), "Factorial of a negative number should be nil")
+    }
+    
 }
